5fbdf45896e6f8f82f349e747302ee96c6ed4da0
ganyong <yong.gan@xc-smart.cn>
2019-06-28 14:12:38 +0800

    修改登录界面停留时间

--- RPT/RPT_LINK_TEST.py
+++ RPT/RPT_LINK_TEST.py
@@ -2,13 +2,13 @@ from selenium import webdriver
 import time
 wd=webdriver.Chrome()
 wd.get('http://localhost:9201/Account/Login?ReturnUrl=%2Foauth%2Fauthorize%3Fredirect_url%3Dhttp%3A%2F%2Flocalhost%3A9200%2F%26client_id%3DXCSmart%26response_type%3Dcode%26direct%3Dtrue')
-time.sleep(3)
+time.sleep(1)
 wd.maximize_window()
 #登陆
 wd.find_element_by_xpath('/html/body/div[3]/form/div[1]/input').send_keys('admin')
 wd.find_element_by_xpath('/html/body/div[3]/form/div[2]/input').send_keys('123qwe')
 wd.find_element_by_xpath('/html/body/div[3]/form/div[3]/button').click()
-time.sleep(3)
+time.sleep(0)
 
 # 在制品数据报表 -- 链接测试
 
